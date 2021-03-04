36c6bdce53ed08fecc8885d167025a21a029a3a6
Xiangmin Mo <xiangminmo6121@gmail.com>
2021-03-04 12:05:08 -0500

    implemented api fetching and display data

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -2,46 +2,106 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-function City(props) {
-  return (<div>This is the City component</div>);
+function City({ location, state, long, lat, population, wages }) {
+  return (
+    <div className="card">
+      <div className="card-header">
+        {location}
+      </div>
+      <div className="card-body">
+        <ul>
+          <li className="card-text">State: {state}</li>
+          <li className="card-text">Location: ({lat},{long})</li>
+          <li className="card-text">Population (estimated): {population}</li>
+          <li className="card-text">Total Wages: {wages}</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-function ZipSearchField({onChange}) {
+function ZipSearchField({ onChange }) {
   return (
-  <div>
-    <label>Zip Code:</label>
-    <input type='text' onChange= {onChange} />
-  </div>
+    <div>
+      <label>Zip Code:</label>
+      <input type='text' onChange={onChange} />
+    </div>
   )
 }
 
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state= {
+    this.state = {
       zipCode: '',
-      cities: []
+      cities: [],
+      isResult: false
     }
   }
 
-  zipChanged(e){
-    this.setState({
+  async zipChanged(e) {
+    await this.setState({
       zipCode: e.target.value
     })
+
+    await fetch(`http://ctp-zip-api.herokuapp.com/zip/${this.state.zipCode}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        if (this.state.isResult){
+          this.setState({
+            isResult: false
+          })
+        }
+        if (!response.ok) {
+          throw new Error('Invalid Zip Code');
+        }
+        return response.json();
+      })
+      .then(success => {
+        this.setState({
+          cities: success,
+          isResult: true
+        })
+      })
+      .catch(error => console.log(error));
   }
 
+  //make the city display dynamically
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField onChange={e => this.zipChanged(e)}/>
+        <ZipSearchField onChange={e => this.zipChanged(e)} />
         <div>
-          <City />
-          <City />
-          <City />
+          {this.state.isResult ?
+            <ul>
+              {
+                this.state.cities.map((item, index) => {
+                  return (
+                    <li style={{ listStyleType: 'none' }} key={index}>
+                      <City
+                        location={item.LocationText}
+                        long={item.Long}
+                        lat={item.Lat}
+                        state={item.State}
+                        population={item.EstimatedPopulation}
+                        wages={item.TotalWages} />
+                    </li>
+                  )
+                }
+                )
+              }
+            </ul>
+            :
+            <h4>No results</h4>
+          }
         </div>
       </div>
     );
