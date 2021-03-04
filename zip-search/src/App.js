a3d14c06dab3666be666e763a390015b85a449fa
Xiangmin Mo <xiangminmo6121@gmail.com>
2021-03-04 09:52:13 -0500

    code from class

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -6,20 +6,40 @@ function City(props) {
   return (<div>This is the City component</div>);
 }
 
-function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+function ZipSearchField({onChange}) {
+  return (
+  <div>
+    <label>Zip Code:</label>
+    <input type='text' onChange= {onChange} />
+  </div>
+  )
 }
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      zipCode: '',
+      cities: []
+    }
+  }
+
+  zipChanged(e){
+    this.setState({
+      zipCode: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField onChange={e => this.zipChanged(e)}/>
         <div>
+          <City />
           <City />
           <City />
         </div>
