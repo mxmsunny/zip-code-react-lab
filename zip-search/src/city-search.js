a86ce41934e5572097d9fe7f68e35d4c94ae74c8
Xiangmin Mo <xiangminmo6121@gmail.com>
2021-03-06 16:44:57 -0500

    added toggling between city search and zip code search, began work on city search

---
+++ zip-search/src/city-search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './zip-search.css';
+
+const CitySearch = () => {
+  return(
+    <div>
+      <div className="App-header">
+        <h2>City Search</h2>
+      </div>
+    </div>
+  );
+}
+
+export default CitySearch;
